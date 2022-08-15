9a4cd9d379d80b2a64663b98201f9305b7a4b152
NyirőTeofil <portrider01234@gmail.com>
2022-08-15 19:49:08 +0200

    completed project and it finaly works at first it scared me that I couldn't do it but in the end it works

--- library.js
+++ library.js
@@ -13,7 +13,140 @@ Book.prototype.info = function() {
      It was written by ${this.author} and has ${this.pages} pages`;
 };
 
-function togleForm() {
+function createElement(type, classN, id, parent) {
+    let parentElement = document.querySelector(`${parent}`);
+
+    let newEl = document.createElement(`${type}`);
+    newEl.classList.add(`${classN}`);
+    newEl.id = `${id}`;
+
+    parentElement.appendChild(newEl);
+}
+
+function removeBook(index) {
+    let book = document.querySelector(`.book-${index}`)
+
+    book.remove();
+
+    library.splice(index, 1);
+}
+
+function changeSatus(index) {
+    let btn = document.querySelector(`.status-button-${index}`);
+    let stBar = document.querySelector(`.status-bar-${index}`);
+    
+    if (library[index].isRead === false) {
+        library[index].isRead = true;
+
+        btn.classList.remove('unread');
+        btn.classList.add('read');
+        btn.textContent = 'read';
+
+        stBar.classList.remove('unread');
+        stBar.classList.add('read');
+
+    } else {
+        library[index].isRead = false;
+
+        btn.classList.remove('read');
+        btn.classList.add('unread');
+        btn.textContent = 'Not read';
+
+        stBar.classList.remove('read');
+        stBar.classList.add('unread');
+
+    }
+
+
+}
+
+Book.prototype.render = function() {
+
+/**For now I add the index to the class, because it is easier to assign text for each specificly*/    
+
+createElement('div', `book-${this.index}`, this.index, '.shelf');
+createElement('div', `status-bar-${this.index}`, this.index, `.book-${this.index}`);
+createElement('p', `book-title-${this.index}`, '', `.book-${this.index}`),
+createElement('p', `book-author-${this.index}`,  '',`.book-${this.index}`);
+createElement('p', `pages-${this.index}`, '', `.book-${this.index}`);
+createElement('div', `btn-group-${this.index}`, '', `.book-${this.index}`);
+document.querySelector(`.book-title-${this.index}`).textContent = `${this.title}`;
+document.querySelector(`.book-author-${this.index}`).textContent = `${this.author}`;
+document.querySelector(`.pages-${this.index}`).textContent = `${this.pages}`;
+
+if (this.isRead === false) {
+    createElement('div', `status-button-${this.index}`, this.index, `.btn-group-${this.index}`);
+    let stButton = document.querySelector(`.status-button-${this.index}`);
+    stButton.classList.add('unread');
+    stButton.textContent = 'Not read';
+
+    stButton.classList.add('change-status')
+
+    let statusBar = document.querySelector(`.status-bar-${this.index}`)
+    statusBar.classList.add('unread');
+
+    /*Button for changing book status*/
+
+    stButton.addEventListener('click', () => changeSatus(this.index))
+
+    /*Button for changing book status*/
+
+} else {
+    createElement('div', `status-button-${this.index}`, this.index, `.btn-group-${this.index}`);
+    let stButton = document.querySelector(`.status-button-${this.index}`);
+    stButton.classList.add('read')
+    stButton.textContent = 'Read';
+
+    stButton.classList.add('change-status')
+
+    let statusBar = document.querySelector(`.status-bar-${this.index}`)
+    statusBar.classList.add('read');
+
+    /*Button for changing book status*/
+
+    stButton.addEventListener('click', () => changeSatus(this.index))
+
+    /*Button for changing book status*/
+
+}
+
+
+
+ createElement('div', `remove-btn-${this.index}`, this.index, `.btn-group-${this.index}`);
+let rmBtn = document.querySelector(`.remove-btn-${this.index}`);
+rmBtn.textContent = 'Remove';
+
+
+/*Button for remove the books from the html and the array*/
+
+    rmBtn.addEventListener('click', () => removeBook(this.index));
+
+/*Button for remove the books from the html and the array*/
+
+
+/**Now I will add another class names for the easier styling */
+
+let title = document.querySelector(`.book-title-${this.index}`);
+let author = document.querySelector(`.book-author-${this.index}`);
+let pages = document.querySelector(`.pages-${this.index}`);
+let book = document.querySelector(`.book-${this.index}`);
+let statusBar = document.querySelector(`.status-bar-${this.index}`);
+let btnGroup = document.querySelector(`.btn-group-${this.index}`)
+
+book.classList.add('book');
+title.classList.add('book-title');
+author.classList.add('book-author');
+pages.classList.add('book-pages');
+statusBar.classList.add('status-bar');
+btnGroup.classList.add('btn-group');
+
+rmBtn.classList.add('remove-btn');
+
+
+}
+
+
+function toggleForm() {
     let formContainer = document.querySelector('.form-container')
 
     if (formContainer.classList.contains('show')) {
@@ -23,30 +156,31 @@ function togleForm() {
     }
 }
 
-function renderBooks(Bookindex) {
-
-}
-
 function submitNewBook() {
-    let formContainer = document.querySelector('.form-container');
+    let title = document.querySelector('#titleInput');
+    let author = document.querySelector('#authorInput');
+    let pages = document.querySelector('#page-number');
+    let isRead = document.querySelector('#isReadCheck');
 
-    let title = document.querySelector('#titleInput').value;
-    let author = document.querySelector('#authorInput').value;
-    let pages = document.querySelector('#page-number').value;
-    let isRead = document.querySelector('#isReadCheck').checked;
-
-    let NewBook = new Book(title, author, pages, isRead, library.length);
+    let NewBook = new Book(title.value, author.value, pages.value, isRead.checked, library.length);
 
     NewBook.prototype = Object.create(Book.prototype);
 
     library.push(NewBook);
 
-    formContainer.classList.remove('show');
+    toggleForm();
+
+    NewBook.render();
+
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    isRead.checked = false;
 }
 
 const addBtn = document.querySelector('.add-btn')
 
-addBtn.addEventListener('click', togleForm);
+addBtn.addEventListener('click', toggleForm);
 
 const submitBook = document.querySelector('#submit-btn')
 
